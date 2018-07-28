4b2df5f8d8afde538336d1a9d594c09015a06dcb
JonPara <Jparadiso91@gmail.com>
2018-07-28 18:55:48 -0400

    Added map onto page with markers. Need GeoJson file and query to continue.

--- logic.js
+++ logic.js
@@ -1,149 +1,38 @@
-// var map = "https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoiam9ucGFyYSIsImEiOiJjamlucjd6YWkwaHphM29yZmN3OHBvd3k5In0.kBpLq9e1DnusW96_EQHdlg"
-// var myMap = L.map('map', {
-//     center: [37.09, -95.71],
-//     zoom: 5
-// });
+var map = L.map( 'map', {
+    center: [20.0, 5.0],
+    minZoom: 2,
+    zoom: 2
+});
 
-// L.tileLayer(map).addTo(myMap);
+L.tileLayer( 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    subdomains: ['a','b','c']
+}).addTo( map );
 
-// var url = "https://data.sfgov.org/resource/gxxq-x39z.json?$limit=10000"
-
-// var marker = L.marker([45.52, -122.67], {
-//     draggable: true,
-//     title: "My First Marker"
-//   }).addTo(myMap);
-  
-//   // Binding a pop-up to our marker
-//   marker.bindPopup("Hello There!");
-
-  // Function to determine marker size based on population
-function markerSize(population) {
-    return population / 40;
-  }
-  
-  // An array containing all of the information needed to create city and state markers
-  var locations = [
+markers = [
     {
-      coordinates: [40.7128, -74.0059],
-      state: {
-        name: "New York State",
-        population: 19795791
-      },
-      city: {
-        name: "New York",
-        population: 8550405
-      }
+      "name": "Canada",
+      "url": "https://en.wikipedia.org/wiki/Canada",
+      "lat": 56.130366,
+      "lng": -106.346771
     },
     {
-      coordinates: [34.0522, -118.2437],
-      state: {
-        name: "California",
-        population: 39250017
-      },
-      city: {
-        name: "Lost Angeles",
-        population: 3971883
-      }
+      "name": "Anguilla",
+      "url": "https://en.wikipedia.org/wiki/Anguilla",
+      "lat": 18.220554,
+      "lng": -63.068615
     },
     {
-      coordinates: [41.8781, -87.6298],
-      state: {
-        name: "Michigan",
-        population: 9928300
-      },
-      city: {
-        name: "Chicago",
-        population: 2720546
-      }
-    },
-    {
-      coordinates: [29.7604, -95.3698],
-      state: {
-        name: "Texas",
-        population: 26960000
-      },
-      city: {
-        name: "Houston",
-        population: 2296224
-      }
-    },
-    {
-      coordinates: [41.2524, -95.9980],
-      state: {
-        name: "Nebraska",
-        population: 1882000
-      },
-      city: {
-        name: "Omaha",
-        population: 446599
-      }
+      "name": "Japan",
+      "url": "https://en.wikipedia.org/wiki/Japan",
+      "lat": 36.204824,
+      "lng": 138.252924
     }
-  ];
-  
-  // Define arrays to hold created city and state markers
-  var cityMarkers = [];
-  var stateMarkers = [];
-  
-  // Loop through locations and create city and state markers
-  for (var i = 0; i < locations.length; i++) {
-    // Setting the marker radius for the state by passing population into the markerSize function
-    stateMarkers.push(
-      L.circle(locations[i].coordinates, {
-        stroke: false,
-        fillOpacity: 0.75,
-        color: "white",
-        fillColor: "white",
-        radius: markerSize(locations[i].state.population)
-      })
-    );
-  
-    // Setting the marker radius for the city by passing population into the markerSize function
-    cityMarkers.push(
-      L.circle(locations[i].coordinates, {
-        stroke: false,
-        fillOpacity: 0.75,
-        color: "purple",
-        fillColor: "purple",
-        radius: markerSize(locations[i].city.population)
-      })
-    );
-  }
-  
-  // Define variables for our base layers
-  var streetmap = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1Ijoiam9ucGFyYSIsImEiOiJjamlucjd6YWkwaHphM29yZmN3OHBvd3k5In0.kBpLq9e1DnusW96_EQHdlg"
-  );
-  var darkmap = L.tileLayer(
-    "https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
-    "access_token=pk.eyJ1Ijoiam9ucGFyYSIsImEiOiJjamlucjd6YWkwaHphM29yZmN3OHBvd3k5In0.kBpLq9e1DnusW96_EQHdlg"
-  );
-  
-  // Create two separate layer groups: one for cities and one for states
-  var states = L.layerGroup(stateMarkers);
-  var cities = L.layerGroup(cityMarkers);
-  
-  // Create a baseMaps object
-  var baseMaps = {
-    "Street Map": streetmap,
-    "Dark Map": darkmap
-  };
-  
-  // Create an overlay object
-  var overlayMaps = {
-    "State Population": states,
-    "City Population": cities
-  };
-  
-  // Define a map object
-  var myMap = L.map("map", {
-    center: [37.09, -95.71],
-    zoom: 5,
-    layers: [streetmap, states, cities]
-  });
-  
-  // Pass our map layers into our layer control
-  // Add the layer control to the map
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
+ ];
+
+for ( var i=0; i < markers.length; ++i ) 
+{
+   L.marker( [markers[i].lat, markers[i].lng] )
+      .bindPopup( '<a href="' + markers[i].url + '" target="_blank">' + markers[i].name + '</a>' )
+      .addTo( map );
+}
